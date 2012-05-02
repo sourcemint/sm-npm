94943da61683c82a12097541ad9b46debbb5c593
cadorn <christoph@christophdorn.com>
2012-05-01 22:54:07 -0700

    better error messages?

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -10,7 +10,8 @@ const ERROR = require("sourcemint-util-js/lib/error");
 
 EXEC("which sm", function(err, stdout, stderr) {
     if (err) {
-        TERM.stdout.writenl("\0red(ERROR: Could not find `sm` on your PATH (" + process.env.PATH + ")! NPM should have installed it.\0)");
+        console.error("ERROR: Could not find `sm` on your PATH (" + process.env.PATH + ")! NPM should have installed it.");
+        TERM.stderr.writenl("\0red(ERROR: Could not find `sm` on your PATH (" + process.env.PATH + ")! NPM should have installed it.\0)");
         process.exit(1);
         return;
     }
@@ -19,7 +20,8 @@ EXEC("which sm", function(err, stdout, stderr) {
     
     EXEC(binPath + " --version", function(err, stdout, stderr) {
         if (err) {
-            TERM.stdout.writenl("\0red(ERROR: '" + err + "' while running `" + binPath + " --version" + "`: " + stderr + "\0)");
+            console.error("ERROR: '" + err + "' while running `" + binPath + " --version" + "`: " + stderr);
+            TERM.stderr.writenl("\0red(ERROR: '" + err + "' while running `" + binPath + " --version" + "`: " + stderr + "\0)");
             process.exit(1);
             return;
         }
@@ -27,7 +29,8 @@ EXEC("which sm", function(err, stdout, stderr) {
         var installedVersion = stdout.replace(/\n$/, "");
         
         if (installedVersion !== process.env.npm_package_version) {
-            TERM.stdout.writenl("\0red(ERROR: Installed `sm` at `which sm` (" + binPath + ") has version '" + installedVersion + "' which is NOT the version '" + process.env.npm_package_version + "' we just installed!\0)");
+            console.error("ERROR: Installed `sm` at `which sm` (" + binPath + ") has version '" + installedVersion + "' which is NOT the version '" + process.env.npm_package_version + "' we just installed!");
+            TERM.stderr.writenl("\0red(ERROR: Installed `sm` at `which sm` (" + binPath + ") has version '" + installedVersion + "' which is NOT the version '" + process.env.npm_package_version + "' we just installed!\0)");
             process.exit(1);
             return;
         }
@@ -50,12 +53,13 @@ EXEC("which sm", function(err, stdout, stderr) {
             TERM.stdout.writenl("\0cyan(chown -R " + process.env.SUDO_UID + ":" + process.env.SUDO_GID + " " + sourcePath + "\0)");
             EXEC("chown -R " + process.env.SUDO_UID + ":" + process.env.SUDO_GID + " " + sourcePath, function(err, stdout, stderr) {
                 if (err) {
-                    TERM.stdout.writenl("\0red(ERROR: '" + err + "' while chowning files!\0)");
+                    console.error("ERROR: '" + err + "' while chowning files!");
+                    TERM.stderr.writenl("\0red(ERROR: '" + err + "' while chowning files!\0)");
                     process.exit(1);
                     return;
                 }
 
-                TERM.stdout.writenl("\0magenta(You should now be able to run `sm` WITHOUT `sudo` for user '" + process.env.SUDO_USER + "'!\0)");
+                TERM.stderr.writenl("\0magenta(You should now be able to run `sm` WITHOUT `sudo` for user '" + process.env.SUDO_USER + "'!\0)");
                 process.exit(0);
             });
         }
